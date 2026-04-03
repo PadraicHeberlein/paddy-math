b18bbd88462fad3fc6d25b789a859ba23ffc941c
PadraicHeberlein <padraic.heb@gmail.com>
2026-04-03 10:30:02 -0400

    refactor: add copyable constraint and fix syntax in Group concept definition

--- algebra_concepts.hpp
+++ algebra_concepts.hpp
@@ -5,11 +5,11 @@ template <typename T>
 concept Scalar = std::is_arithmetic_v<T>; // A basic rule: T must be a number
 
 template <typename T>
-concept Group = requires(T a, T b) {
+concept Group = std::copyable<T> && requires(const T a, const T b) {
   { a + b } -> std::same_as<T>;
   { a - b } -> std::same_as<T>;     // Closure
   { T::zero() } -> std::same_as<T>; // Identity
   { -a } -> std::same_as<T>;        // Inverse
   { a == b } -> std::same_as<bool>; // Equality
   { a != b } -> std::same_as<bool>; // Inequality
-}
+};

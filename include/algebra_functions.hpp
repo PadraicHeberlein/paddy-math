64e7d4df5eb1ad8c0a36f888a52e2f2438aac181
PadraicHeberlein <padraic.heb@gmail.com>
2026-04-06 12:06:21 -0400

    refactor: reformat exp_taylor implementation and fix logic error in Taylor series loop

--- include/algebra_functions.hpp
+++ include/algebra_functions.hpp
@@ -4,29 +4,29 @@
 #include "algebra_concepts.hpp"
 #include "complex.hpp"
 #include "matrix.hpp"
-#include <cmath>
 #include <cassert>
+#include <cmath>
 
 /**
  * @brief Generic Exponential function using Taylor Series expansion.
  * Suitable for any type satisfying the Algebra concept.
- * 
+ *
  * @tparam T A type that satisfies Algebra (Ring and a Vector Space)
  * @param x The input value
  * @param terms Number of terms in the Taylor series (default 50)
  * @return T The computed value of e^x
  */
-template <Algebra T>
-T exp_taylor(const T& x, int terms = 50) {
-    T res = T(1); 
-    T current_term = T(1);
-    
-    for (int i = 1; i <= terms; ++i) {
-        current_term = (current_term * x) * (1.0 / i);
-        res = res + current_term;
-    }
-    
-    return res;
+template <Algebra T> T exp_taylor(const T &x, int terms = 50) {
+  T res = T(1);
+  T current_term = T(1);
+
+  for (int i = 1; i <= terms; ++i) {
+    current_term = (current_term * x) * (1.0 / i);
+    res = res + current_term;
+    current_term = T(1);
+  }
+
+  return res;
 }
 
 /**
@@ -34,9 +34,11 @@ T exp_taylor(const T& x, int terms = 50) {
  * that don't have a specialized analytical exp.
  */
 template <Algebra T>
-requires (!std::is_same_v<T, Complex> && !std::is_same_v<T, Matrix<typename T::value_type>>) // Avoid ambiguity
-T exp_generic(const T& x) {
-    return exp_taylor(x);
+  requires(
+      !std::is_same_v<T, Complex> &&
+      !std::is_same_v<T, Matrix<typename T::value_type>>) // Avoid ambiguity
+T exp_generic(const T &x) {
+  return exp_taylor(x);
 }
 
 #endif // ALGEBRA_FUNCTIONS_HPP
